f13ef9add6c0f04906757744f8d61ff0efe1effd
wiseguy16 <gergusa04@gmail.com>
2016-07-28 15:51:59 -0400

    Initial Commit

---
+++ HighVoltage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighVoltage
+//
+//  Created by Gregory Weiss on 7/28/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighVoltage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HighVoltage
+//
+//  Created by Gregory Weiss on 7/28/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
